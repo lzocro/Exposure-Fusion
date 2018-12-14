00d2471251a7afbdb8c5686cfb3d05d2ac7e0414
Lorenzo Croissant <32764330+lzocro@users.noreply.github.com>
2018-12-14 14:59:10 +0100

    Update script.py
    added a function to read the input sequence to an array from a list of filenames

--- script.py
+++ script.py
@@ -23,12 +23,38 @@ import cv2
 #Read Image works for most formats but NOT RAW
 img = cv2.imread('img_forest.jpg')
 
+#Function to import a sequence on pictures into an array
+def read_sequence_to_fuse(seq):
 
-#Display Image (in real pixel size, so HD images will be wider than the screen... not sure how to fix )
+	'''
+
+	seq should be a list of file names such as ["img1.jpg","img2.jpg"]
+	The images should have the same dimensions
+
+	'''
+
+	n_images = len(seq)
+	if n_images == 1:
+		warnings.warn('sequence only has one image', Warning)
+
+	height_images = np.size(cv2.imread(seq[0]),0)
+	width_images = np.size(cv2.imread(seq[0]),1)
+	output_object = np.empty(n_images, dtype='object')#np.empty((n_images, height_images, width_images, 3)) #3 is for RGB
+
+	for i in range(len(seq)):
+		output_object[i] = cv2.imread(seq[i])
+
+	shapes = [i.shape for i in output_object]
+	assert shapes[1:] == shapes[:-1], 'All images do not have the same shape' #checks that all images have the same dimensions
+	return(output_object)
+
+#example 
+img= read_sequence_to_fuse(['img_forest.jpg','img_forest.jpg'])
 
 cv2.namedWindow('title', cv2.WINDOW_NORMAL) #creates a resizeable frame for large pictures, can be skipped
-cv2.imshow('title',img) #'title' is title of the window and the id to refer to it (see namedWindow)
-cv2.waitKey(0) #pressing any key continues the program ...
+for i in range(len(img)):
+	cv2.imshow('title',img[i]) #'title' is title of the window and the id to refer to it (see namedWindow)
+	cv2.waitKey(0) #pressing any key continues the program ...
 cv2.destroyAllWindows()# ... and closes the window
 
 #Applying Grayscale filter to image
